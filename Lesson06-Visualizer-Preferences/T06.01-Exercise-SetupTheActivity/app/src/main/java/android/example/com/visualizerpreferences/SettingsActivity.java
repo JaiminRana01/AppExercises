3312ecd6c0ea4472324af05de7936810f998d6c0
JaiminRana01 <jaiminr625@gmail.com>
2021-05-03 16:56:58 +0530

    Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity completed

---
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,14 @@
+package android.example.com.visualizerpreferences;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// COMPLETED (1) Create a new Empty Activity named SettingsActivity; make sure to generate the
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
